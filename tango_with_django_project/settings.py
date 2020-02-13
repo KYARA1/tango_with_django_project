228b36d19b840091835f3983a42e2eee27ef485c
KYARA1 <2402519L@student.gla.ac.uk>
2020-02-13 15:01:40 +0000

    chapter 9 commit

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -128,3 +128,10 @@ STATIC_URL = '/static/'
 # Media files
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+PASSWORD_HASHERS = (
+'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
+LOGIN_URL = 'rango:login'
